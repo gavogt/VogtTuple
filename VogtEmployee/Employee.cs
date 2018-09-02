b0e1e485fa3e6b742406d0721598a1afec35e037
gavogt <gvogt@asu.edu>
2018-09-02 15:26:49 -0700

    Add project files.

---
+++ VogtEmployee/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VogtEmployee
+{
+    class Employee
+    {
+        // Propfull
+        private int empNumber;
+
+        public int EmpNumber
+        {
+            get { return empNumber; }
+            set { empNumber = value; }
+        }
+
+        private string empFirstName;
+
+        // Propfull
+        public string EmpFirstName
+        {
+            get { return empFirstName; }
+            set { empFirstName = value; }
+        }
+
+        // Propfull
+        private string empLastName;
+
+        public string EmpLastName
+        {
+            get { return empLastName; }
+            set { empLastName = value; }
+        }
+    }
+}
---
+++ VogtEmployee/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VogtEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = default;
+
+            DisplayHeader();
+
+            Employee emp = new Employee();
+
+            while(choice != 1)
+                Console.WriteLine("What is the number of the employee?");
+                emp.EmpNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("What is the first name of the employee?");
+                emp.EmpFirstName = Console.ReadLine();
+
+                Console.WriteLine("What is the last name of the employee?");
+                emp.EmpLastName = Console.ReadLine();
+
+            var listOfEmployees = new List<(int empNo, string empFName, string empLName)>();
+
+            listOfEmployees.Add((emp.EmpNumber, emp.EmpFirstName, emp.EmpLastName));
+
+            foreach (var employee in listOfEmployees)
+            {
+                Console.WriteLine($"Employee number is: {employee.empNo}");
+                Console.WriteLine($"Employee name is: {employee.empFName} {employee.empLName}");
+            }
+
+        }
+
+        #region DisplayHeader
+        /// <summary>
+        /// Displays a welcome screen to the user
+        /// </summary>
+        public static void DisplayHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("Welcome to the Employee List program!\n");
+
+            Console.ResetColor();
+
+        }
+        #endregion
+    }
+}
